a87860043b258cb4d440ff3be5754b112d8e4be3
catalin.cimpoeru <catalin.cimpoeru@ROM-CCATALIN.asentinel.com>
2016-06-17 17:27:04 +0300

    Repair Remote

---
+++ src/main/java/construction/pm/lib/rmi/AbstractEmployeePositionRemote.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package construction.pm.lib.rmi;
+
+import construction.pm.lib.dto.EmployeePositionDTO;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * @author catalin.cimpoeru
+ */
+public interface AbstractEmployeePositionRemote extends Remote{
+    public boolean addPosition(EmployeePositionDTO employeePositionDTO) throws RemoteException;
+    public boolean removePosition(EmployeePositionDTO employeePositionDTO) throws RemoteException;
+}
--- src/main/java/construction/pm/lib/rmi/AbstractUserRemote.java
+++ src/main/java/construction/pm/lib/rmi/AbstractUserRemote.java
@@ -3,15 +3,16 @@ package construction.pm.lib.rmi;
 
 import construction.pm.lib.dto.UserDTO;
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 /**
  *
  * @author catalin.cimpoeru
  */
 public interface AbstractUserRemote extends Remote{
-    public boolean register(UserDTO user);
-    public UserDTO logIn(UserDTO user);
-    public UserDTO findByUsername(String username);
-    public void removeUser(UserDTO userDto);
+    public boolean register(UserDTO user) throws RemoteException;
+    public UserDTO logIn(UserDTO user) throws RemoteException;
+    public UserDTO findByUsername(String username) throws RemoteException;
+    public void removeUser(UserDTO userDto) throws RemoteException;
     
 }

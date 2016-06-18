8677550299c0ed9218dbca40de8c29296031cf5d
catalin.cimpoeru <catalin.cimpoeru@ROM-CCATALIN.asentinel.com>
2016-06-18 10:39:02 +0300

    getAll

--- src/main/java/construction/pm/lib/rmi/AbstractEmployeePositionRemote.java
+++ src/main/java/construction/pm/lib/rmi/AbstractEmployeePositionRemote.java
@@ -8,6 +8,7 @@ package construction.pm.lib.rmi;
 import construction.pm.lib.dto.EmployeePositionDTO;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.List;
 
 /**
  * @author catalin.cimpoeru
@@ -15,4 +16,5 @@ import java.rmi.RemoteException;
 public interface AbstractEmployeePositionRemote extends Remote{
     public boolean addPosition(EmployeePositionDTO employeePositionDTO) throws RemoteException;
     public boolean removePosition(EmployeePositionDTO employeePositionDTO) throws RemoteException;
+    public List<EmployeePositionDTO> getAllEmployeePositions() throws RemoteException;
 }

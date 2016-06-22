38efc0e7cd36477cb10dd6cc413e137febc90f1a
catalin.cimpoeru <catalin.cimpoeru@ROM-CCATALIN.asentinel.com>
2016-06-22 10:34:27 +0300

    add layers remote

---
+++ src/main/java/construction/pm/lib/rmi/AbstractLayersRemote.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package construction.pm.lib.rmi;
+
+import construction.pm.lib.dto.ProjectLayerDTO;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+/**
+ *
+ * @author catalin.cimpoeru
+ */
+public interface AbstractLayersRemote extends Remote {
+    
+    public boolean addLayer(ProjectLayerDTO layer) throws RemoteException;
+    public List<ProjectLayerDTO> getAllLayers() throws RemoteException; 
+}

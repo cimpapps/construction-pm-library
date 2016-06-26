1dcad27a5f6457911d8bc52e5162320bb9893d13
cimpapps <catalincimpoeru24@gmail.com>
2016-06-27 01:09:51 +0300

    get project layers

--- src/main/java/construction/pm/lib/dto/ProjectLayerDTO.java
+++ src/main/java/construction/pm/lib/dto/ProjectLayerDTO.java
@@ -50,5 +50,10 @@ public class ProjectLayerDTO implements Serializable {
         this.projectsId = projectsId;
     }
 
+    @Override
+    public String toString() {
+        return name;
+    }
 
+    
 }
